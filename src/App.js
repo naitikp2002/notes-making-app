dfd034558e6a03228beb087fe97a6a2f78921381
naitikp2002 <naitikpatel2002@gmail.com>
2021-12-13 17:07:25 +0530

    Add nevigation bar from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './NavBar';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Naitik Patel
-        </a>
-      </header>
+    <div>
+      <>
+      <NavBar/>
+      My name is  naitik patel i m going to create a notes app
+    </>
     </div>
   );
 }
